4d16478ae5729253ef11a21cc31ac5247e91988c
Marc Rutkowski <marc@attractive-media.fr>
2017-02-01 16:51:25 +0100

    Add example with Flow type annotations

---
+++ example/UserDetails.js
@@ -0,0 +1,44 @@
+/* @flow */
+import React from 'react';
+
+export type User = {
+  id: number,
+  name: string,
+  country?: string,
+};
+
+type Props = {
+  /** User info */
+  user: User,
+  /** Some number */
+  num: number,
+  /** Optional property */
+  option?: string,
+  /** Optional callback */
+  func?: (value: string) => void,
+  /** Optional array of users */
+  friends?: Array<User>
+};
+
+/** Render user details */
+class UserDetails extends React.Component {
+  props: Props;
+
+  static defaultProps = {
+    option: 'foo',
+  };
+
+  render() {
+    const { user, num, option } = this.props;
+    return (
+      <div>
+        <p>Id: {user.id}</p>
+        <p>Name: {user.name}</p>
+        <p>Num: {num}</p>
+        {option && <p>Option: {option}</p>}
+      </div>
+    );
+  }
+}
+
+export default UserDetails;
