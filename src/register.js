6cb1f7f61c2ea03d5f4311c020efbce40536164e
Marc Rutkowski <marc@attractive-media.fr>
2017-03-03 17:07:27 +0100

    Change tab order to display Story before Props panel

--- src/register.js
+++ src/register.js
@@ -7,13 +7,13 @@ const ADDON_ID = 'attractivemedia/storybook-addon-props';
 addons.register(ADDON_ID, (api) => {
   const channel = addons.getChannel();
 
-  addons.addPanel(`${ADDON_ID}/propsPanel`, {
-    title: 'Props',
-    render: () => <PropsPanel channel={channel} onStory={api.onStory} />
-  });
-
   addons.addPanel(`${ADDON_ID}/storyPanel`, {
     title: 'Story',
     render: () => <StoryPanel channel={channel} onStory={api.onStory} />
+  });
+
+  addons.addPanel(`${ADDON_ID}/propsPanel`, {
+    title: 'Props',
+    render: () => <PropsPanel channel={channel} onStory={api.onStory} />
   });
 });
